cad2124c12e0f88b8bd025841c4cd806af90f598
lidemin-recall <1425889927@qq.com>
2020-02-21 00:19:35 +0800

    webpack

---
+++ webpack/src/index.js
@@ -0,0 +1,16 @@
+/**
+ * 集中在main.js中导入外部资源
+ * 减少了页面请求次数
+ * 
+ *  */
+
+//  ES6导入模块的方式：import *** from ***
+// 存在浏览器兼容问题，浏览器不支持
+import $ from 'jquery'
+
+$(function () {
+    $('li:odd').css('backgroundColor', 'green')
+    $('li:even').css('backgroundColor', () => {
+        return '#' + 'A99634'
+    })
+})
---
+++ webpack/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html'
+})
+module.exports = {
+    // 编译模式
+    mode: 'development', //开发环境
+    // mode: 'production' //生产环境
+    // 手动指定打包的入口和出口
+    entry: path.join(__dirname, './src/index.js'),
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [htmlPlugin]
+}
